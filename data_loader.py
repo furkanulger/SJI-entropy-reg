2d580e0515876d994d69dafae8d59bb46365f48f
Furkan Ulger <furkan.ulger@outlook.com>
2023-01-10 22:36:27 +0300

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -3,14 +3,14 @@ from torchvision import datasets, transforms
 
 def dataloader(train_batch_size):
     train_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('data_supervised_shuffled/train/', transforms.Compose([
+        datasets.ImageFolder('dataset/train/', transforms.Compose([
             transforms.Resize((84, 84)),  # width, height
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.618, 0.6373, 0.633), std=(0.2538, 0.2394, 0.2427))
         ])), batch_size=train_batch_size, num_workers=0, shuffle=True)
 
     valid_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('data_supervised_shuffled/validation/', transforms.Compose([
+        datasets.ImageFolder('dataset/validation/', transforms.Compose([
             transforms.Resize((84, 84)),  # width, height
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
@@ -18,7 +18,7 @@ def dataloader(train_batch_size):
         ])), batch_size=train_batch_size, num_workers=0, shuffle=True)
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder('data_supervised_shuffled/test/', transforms.Compose([
+        datasets.ImageFolder('dataset/test/', transforms.Compose([
             transforms.Resize((84, 84)),  # width, height
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.618, 0.6373, 0.633), std=(0.2538, 0.2394, 0.2427))
